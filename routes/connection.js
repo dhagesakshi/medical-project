ba13b948e57f7e133e6bd3210bd850157358cdf0
dhagesakshi <dhagesakshi433@gmail.com>
2026-05-19 12:17:01 +0530

    Configured Railway database

--- routes/connection.js
+++ routes/connection.js
@@ -3,10 +3,19 @@ var mysql = require('mysql')
 var util = require('util')
 
 var conn = mysql.createConnection({
-    host: 'mysql',
-    user: 'root',
-    password: 'root',
-    database: 'medical_db'
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT
+})
+
+conn.connect((err) => {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log("MySQL Connected")
+    }
 })
 
 var exe = util.promisify(conn.query).bind(conn)
